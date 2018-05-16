e07f56bfd8634e11e83c929af1d87c95693383f3
jerry_p <abrcdf1023@gmail.com>
2018-05-16 13:05:46 +0800

    add description and customiz newbranch

--- bin/penguin-git-cli.js
+++ bin/penguin-git-cli.js
@@ -10,26 +10,44 @@ const {
   branchDone,
 } = require('../lib')
 
+/**
+ * new branch, Create a new branch/
+ * branch done, Merge current branch into develop branch and update it version.
+ * release, Merge develop branch into release branch and update it version.
+ * product deploy, Merge release branch into master and update it version.
+ * roll back latest remote commit, This is a UNSAFE option. Please use it cautiously.
+ */
 const defaultQuestions = [{
   type: 'list',
   name: 'gitCommand',
   message: 'Select Command ...',
-  choices: ['new branch', 'branch done', 'release', 'product deploy', '(UN_SAFE) roll back latest remote commit']
+  choices: ['new branch', 'branch done', 'release', 'product deploy', 'roll back latest remote commit']
 }]
 
+/**
+ * bug, Create a branch with bug_ prefix.
+ * feature, Create a branch with feature_ prefix.
+ * refactor, Create a branch with refactor_ prefix.
+ * customize, Create a branch without any prefix.
+ */
 const newBranchQuestions = [{
   type: 'list',
-  name: 'type',
+  name: 'branchType',
   message: 'Please select branch type ...',
-  choices: ['bug', 'feature', 'refactor']
+  choices: ['bug', 'feature', 'refactor', 'customize']
 }, {
   name: 'name',
   message: 'Please key in branch name'
 }]
 
+/**
+ * major, ex: v1.1.0 -> v2.0.0
+ * minor, ex: v1.0.1 -> v1.1.0
+ * patch, ex: v1.0.0 -> v1.0.1
+ */
 const branchDoneQuestions = [{
   type: 'list',
-  name: 'type',
+  name: 'updateType',
   message: 'Please select update type ...',
   choices: ['major', 'minor', 'patch']
 }]
