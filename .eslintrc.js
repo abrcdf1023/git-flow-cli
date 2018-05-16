1a58a5d14edc7125bf0a0ccf9f4ae8f42994992d
jerry_p <abrcdf1023@gmail.com>
2018-05-16 18:20:41 +0800

    fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     'node': true
   },
   'parserOptions': {
-    'ecmaVersion': 6,
+    'ecmaVersion': 2017,
     'sourceType': 'module',
     'ecmaFeatures': {
       'impliedStrict': true,
